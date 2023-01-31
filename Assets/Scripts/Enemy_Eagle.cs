a0087d082e6f58544d4a01cea2c5e4dcb1177828
2ez4rtz <ggg573608@gmail.com>
2023-01-31 01:59:07 -0800

    Add eagle->attacking animation and attack range
    BUG1. facing issues , 冲锋的时候兔子在鹰上面鹰的角度不对

--- Assets/Scripts/Enemy_Eagle.cs
+++ Assets/Scripts/Enemy_Eagle.cs
@@ -14,7 +14,17 @@ public class Enemy_Eagle : Enemy
 
     private bool isUp = true;
 
-    
+    // public GameObject player;
+
+    //chase player
+    Transform target;
+    Vector2 moveDirection;
+
+    //chasing angle
+    //序列化 定义之后 虽然是private 但是会在inspector里面出现 可以修改
+    [SerializeField] private float facingAngle;
+
+
 
     // Start is called before the first frame update
     protected override void Start()
@@ -28,13 +38,15 @@ public class Enemy_Eagle : Enemy
         BottomY = bottom.position.y;
         Destroy(top.gameObject);
         Destroy(bottom.gameObject);
-
+        //catch the player pos.
+        target = GameObject.Find("player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        checkPlayerLocation();
     }
 
     void Movement()
@@ -56,7 +68,83 @@ public class Enemy_Eagle : Enemy
             }
         }
     }
+
+    void checkPlayerLocation()
+    {
+        if (Mathf.Abs(target.transform.position.x - transform.position.x) <= 15)
+        {
+            if (target)
+            {
+                Vector2 direction = (target.position - transform.position).normalized;
+                //calcuate angle  , 三角函数
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                //转向角度
+                // rb.rotation = angle;
+                if (transform.position.x < target.position.x)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                moveDirection = direction;
+                rb.velocity = new Vector2(direction.x, direction.y) * Speed;
+                //if close to 15 , charing 
+                rb.rotation = 0;
+                //charging mode
+                if (Mathf.Abs(target.position.x - transform.position.x) <= 5)
+                {
+                    rb.velocity = new Vector2(direction.x, direction.y) * Speed * 4f;
+                    anim.SetBool("attack", true);
+                    if ((transform.position.x < target.position.x) && (transform.position.y < target.position.y))
+                    {
+                        rb.rotation = facingAngle;
+                        //skip the fight
+                    }
+                    else if ((transform.position.x > target.position.x) && (transform.position.y > target.position.y))
+                    {
+                        rb.rotation = -facingAngle;
+                    }
+                    else
+                    {
+                        rb.rotation = 0;
+                    }
+                }
+                else
+                {
+                    anim.SetBool("attack", false);
+                }
+            }
+        }
+    }
 }
+
+
+    // void ChasePlayer()
+    // {
+    //     Debug.Log("123");
+    //     if(Mathf.Abs(player.transform.position.x-transform.position.x) <= 15){
+
+    //         anim.SetBool("attack",true);
+    //         // isAttacking=true;
+
+    //         // Eagleattack();
+    //     }else{
+    //         // isAttacking=false;
+    //         // Eagleattack();
+    //         anim.SetBool("attack",false);
+    //     }
+    // }
+    // void Eagleattack()
+    // {
+    //     if(isAttacking){
+    //         anim.SetBool("attack",true);
+    //     }
+    //     else{
+    //         anim.SetBool("attack",false);
+    //     }
+    // }
 
 
 

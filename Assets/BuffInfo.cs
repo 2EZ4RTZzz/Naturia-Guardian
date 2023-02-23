59814345af386ec222e76d37fb03a6195b96b43d
Archer001201 <2111410956@qq.com>
2023-02-23 14:31:38 -0800

    合成台UI
    1. 添加了合成buff详情页面

---
+++ Assets/BuffInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Shameless.Inventory;
+
+public class BuffInfo : MonoBehaviour
+{
+    public int itemID;
+    public ItemDetails itemDetails;
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI buffName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemDetails = InventoryManager.Instance.GetItemDetails(itemID);
+        icon.sprite = itemDetails.itemIcon;
+        buffName.text = itemDetails.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

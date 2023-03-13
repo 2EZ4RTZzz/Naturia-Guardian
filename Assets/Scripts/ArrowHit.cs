08d9e3990aaa79cd731e8b394ca8f8814ebf5c63
2ez4rtz <ggg573608@gmail.com>
2023-03-12 17:35:35 -0700

    arrow

--- Assets/Scripts/ArrowHit.cs
+++ Assets/Scripts/ArrowHit.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class ArrowHit : MonoBehaviour
 {
     public GameObject arrowPrefab;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attack();
     }
 
-
+    void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Shoot();
+            Debug.Log("test123");
+            // anim.SetTrigger("attack_1");
+            // StartCoroutine(StartAttack());
+        }
+    }
     void Shoot()
     {
-        Instantiate(arrowPrefab,transform.position,transform.rotation);
+
+        Instantiate(arrowPrefab, transform.position, transform.rotation);
     }
 
 }

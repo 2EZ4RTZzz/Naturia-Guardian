1965fa8cdb97e8ecc30cab13ac089cb92eff3581
2ez4rtz <ggg573608@gmail.com>
2023-03-14 11:32:48 -0700

    feedback

--- Assets/Scripts/Enemy_Eagle.cs
+++ Assets/Scripts/Enemy_Eagle.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Enemy_Eagle : Enemy
 {
     private Rigidbody2D rb;
-    // private Collider2D Coll;
+    private Collider2D Coll;
     public Transform top, bottom;
     //locked 1
     private float Speed = 1;
     private float TopY, BottomY;
-    // private Animator anim;
+    private Animator anim;
 
     private bool isUp = true;
 
-    // public GameObject player;
+    public GameObject player;
 
     //chase player
     Transform target;
@@ -32,8 +32,8 @@ public class Enemy_Eagle : Enemy
         //super the componet.
         base.Start();
         rb = GetComponent<Rigidbody2D>();
-        // Coll = GetComponent<Collider2D>();
-        // anim = GetComponent<Animator>();
+        Coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
         TopY = top.position.y;
         BottomY = bottom.position.y;
         Destroy(top.gameObject);
@@ -78,7 +78,7 @@ public class Enemy_Eagle : Enemy
                 //calcuate angle  , 三角函数
                 float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                 //转向角度
-                // rb.rotation = angle;
+                rb.rotation = angle;
                 if (transform.position.x < target.position.x)
                 {
                     transform.localScale = new Vector3(-1, 1, 1);

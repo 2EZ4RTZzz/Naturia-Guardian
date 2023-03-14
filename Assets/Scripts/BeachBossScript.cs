65820f76559e88cef0bc3c675aeb5ec17d44d55b
2ez4rtz <ggg573608@gmail.com>
2023-03-14 01:47:50 -0700

    boss

--- Assets/Scripts/BeachBossScript.cs
+++ Assets/Scripts/BeachBossScript.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class BeachBossScript : Enemy
 {
     public float speed;
-    public float waitTime;
-    
+    public float startWaitTime;
+    private float waitTime;
+
+    public Transform movePos;
+    public Transform leftDownPos;
+    public Transform rightUpPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
+        waitTime = startWaitTime;
+        movePos.position = GetRandomPos();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //调用父类的Update()方法
+        base.update();
+
+        transform.position = Vector2.MoveTowards(transform.position, movePos.position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, movePos.position) < 0.1f)
+        {
+            if(waitTime <= 0)
+            {
+                movePos.position = GetRandomPos();
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
+
+    Vector2 GetRandomPos()
+    {
+        Vector2 rndPos = new Vector2(Random.Range(leftDownPos.position.x, rightUpPos.position.x), Random.Range(leftDownPos.position.y, rightUpPos.position.y));
+        return rndPos;
+    }
+
 }

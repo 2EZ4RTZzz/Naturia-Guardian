9aeb849a577f858829ee8c8fcd18b5a482717911
2ez4rtz <ggg573608@gmail.com>
2023-03-16 03:59:48 -0700

    second spell , bug , right side is good , left side not work

---
+++ Assets/Scripts/Player/Sickle.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sickle : MonoBehaviour
+{
+    //flying speed.
+    public float speed;
+    //damage 
+    public int damage;
+    //rotate speed
+    public float rotateSpeed;
+
+    public float tuning;
+
+    private Rigidbody2D rb2d;
+
+    private Transform playerTransform;
+    private Transform sickleTransform;
+    private Vector2 startSpeed;
+    private CameraShake camShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d=GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.right * speed;
+        startSpeed = rb2d.velocity; 
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        sickleTransform = GetComponent<Transform>();
+        camShake = GameObject.FindGameObjectWithTag("CameraShake").GetComponent<CameraShake>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateSpeed);
+        //follow the player current location
+
+        float y = Mathf.Lerp(transform.position.y, playerTransform.position.y, tuning);
+        transform.position = new Vector3(transform.position.x, y, 0.0f);
+        rb2d.velocity = rb2d.velocity - startSpeed * Time.deltaTime;
+
+        //destory the sickle.
+        if(Mathf.Abs(transform.position.x - playerTransform.position.x) < 0.5f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+           other.GetComponent<Enemy>().TakeDamge(damage);
+        }
+    }
+}

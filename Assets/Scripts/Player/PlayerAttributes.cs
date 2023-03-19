0ec7aab7cfdcea0eddaf6c0e031275bbb18d1005
Archer001201 <2111410956@qq.com>
2023-03-18 18:31:53 -0700

    暴击buff

--- Assets/Scripts/Player/PlayerAttributes.cs
+++ Assets/Scripts/Player/PlayerAttributes.cs
@@ -13,16 +13,29 @@ public class PlayerAttributes : MonoBehaviour
     public float currentHP;
     public float currentMP;
 
+    [Header("Buff List")]
+    public BuffState_SO buffs;
+
+    private float oriCrit;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHP = maxHP;
         currentMP = maxMP;
+        oriCrit = crit;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        for (int i=0; i<buffs.buffList.Count; i++)
+        {
+            InventoryItem buff = buffs.buffList[i];
+            if (buff.itemID == 1010)
+            {
+                crit = oriCrit + 10 * buff.itemAmount;
+            }
+        }
     }
 }
